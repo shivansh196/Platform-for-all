f18a1116c08513a756fd6a5fa83c6da0ee3cb471
shivansh196 <ipshivansh@gmail.com>
2024-03-27 12:55:57 +0530

    Adding apply job and create job funtions

--- contracts/ContractForFreelancing.sol
+++ contracts/ContractForFreelancing.sol
@@ -49,3 +49,62 @@ contract FreelancingContract {
     /**
      * @dev A mapping of an address to an unsigned integer to show how many times an address has been paid;
      */
+
+      mapping(address => uint) private s_timesHasBeenPaid;
+    uint256 private s_numberOfListedJobs;
+
+    
+    // Modifiers
+    modifier onlyEmployer(uint256 id) {
+        Job memory job = s_Jobs[id];
+        if(msg.sender != job.employer){
+            revert FreelancingBasicContract__NotEmployer();
+        }
+        _;
+    }
+
+    modifier onlyEmployee(uint256 id) {
+        Job memory job = s_Jobs[id];
+        if(msg.sender != job.employee){
+            revert FreelancingBasicContract__NotEmployee();
+        }
+        _;
+    }
+
+    /**
+     * @dev This is the function to create new jobs
+     * @dev Updates most of the struct
+     * @param _jobTitle The title of the job that the employer needs
+     * @param _jobDescription The description/requirements of the job
+     * @return Return the number of the specific listed job so we can use it as an ID
+     */
+    function createJob(string memory _jobTitle, string memory _jobDescription) external returns(uint256) {
+        Job storage job = s_Jobs[s_numberOfListedJobs];
+
+        job.employer = msg.sender;
+        job.jobTitle = _jobTitle;
+        job.jobDescription = _jobDescription;
+        s_numberOfListedJobs ++;
+
+        return s_numberOfListedJobs - 1;
+    }
+    /**
+     * @dev This function is used by employees to apply to a specific job
+     * @param _id The id of the specific job listed
+     */
+    function applyJob(uint _id) external {
+        Job storage job = s_Jobs[_id];
+        if(s_pending[msg.sender]) {
+            revert FreelancingBasicContract__AlreadyApplied();
+        }   
+        if(msg.sender == job.employer) {
+            revert FreelancingBasicContract__EmployerCantEmployHimself();
+        }    
+        s_pending[msg.sender] = true;
+        job.pending = msg.sender;
+    }
+    /**
+     * @dev The function used by the creator of the job to accept a caditate
+     * @param _candidate The address of the person who applied to a job
+     * @param _id The id of the specific job
+     */
